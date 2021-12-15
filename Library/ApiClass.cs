96421cc181ab23813fa7a8c354b56e19f02d3e60
Олег Низамов <35757313+olegnizamov@users.noreply.github.com>
2021-12-15 18:09:29 +0400

    Функциональная инъекция зависимостей

---
+++ Library/ApiClass.cs
@@ -0,0 +1,42 @@
+using System;
+using System;
+
+namespace clearArhitecture.Library
+{
+    public class ApiClass
+    {
+        private RobotCleaner _robot = new RobotCleaner();
+        private State _robotState = new State();
+
+        private void TransferToCleaner(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Move(int meters, State state, Action<string> transferDelegate, Action<int, State, Action<string>> moveFunction)
+        {
+            moveFunction(meters, state, transferDelegate);
+        }
+
+        public void Turn(int angleDegrees, State state, Action<string> transferDelegate, Action<int, State, Action<string>> turnFunction)
+        {
+            turnFunction(angleDegrees, state, transferDelegate);
+        }
+
+        public void Set(Tools tool, State state, Action<string> transferDelegate, Action<Tools, State, Action<string>> setFunction)
+        {
+            setFunction(tool, state, transferDelegate);
+        } 
+        
+        public void Start(State state, Action<string> transferDelegate, Action<State, Action<string>> startFunction)
+        {
+            startFunction(state, transferDelegate);
+        }
+        
+        public void Stop(Action<string> transferDelegate, Action<Action<string>> stopFunction)
+        {
+            stopFunction(transferDelegate);
+        }
+
+    }
+}
